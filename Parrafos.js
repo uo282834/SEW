bcaf35c11d0014c71e9c30bd3c8353f95f1c81e7
uo282834 <113586636+uo282834@users.noreply.github.com>
2022-11-22 22:54:47 +0100

    Delete Parrafos.js

--- Parrafos.js
+++
@@ -1,9 +0,0 @@
-document.write("<p>Curso actual: ");
-document.write(asignatura.curso);
-document.write("</p>");
-document.write("<p>Estudiante: ");
-document.write(asignatura.estudiante);
-document.write("</p>");
-document.write("<p>e-mail: ");
-document.write(asignatura.email);
-document.write("</p>");
