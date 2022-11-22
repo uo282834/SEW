771e9628368762876369845d609fcaea237f49c5
uo282834 <113586636+uo282834@users.noreply.github.com>
2022-11-22 22:49:31 +0100

    Add files via upload

---
+++ Cabecera.js
@@ -0,0 +1,8 @@
+var asignatura = new Object();
+asignatura.nombre = "Software y estándares para la Web";
+asignatura.titulacion = "Grado en Ingeniería Informática del Software";
+asignatura.centro = "Escuela de Ingeniería Informática";
+asignatura.universidad = "Universidad de Oviedo";
+asignatura.curso = "2022-23";
+asignatura.estudiante = "Alonso Antuña Gómez";
+asignatura.email = "UO282834@uniovi.es";
---
+++ Titulo3.js
@@ -0,0 +1,3 @@
+document.write("<h3>");
+document.write(asignatura.centro);
+document.write("</h3>");
