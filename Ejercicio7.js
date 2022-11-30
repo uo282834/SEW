32f92ec6ff1e3c4ab243c4f782155ef0f9620bb6
uo282834 <113586636+uo282834@users.noreply.github.com>
2022-11-30 18:10:45 +0100

    Add files via upload

---
+++ Ejercicio7.js
@@ -0,0 +1,56 @@
+"use strict";
+class Computacion{
+    constructor()
+    {
+    }
+    ocultar(value){
+        $(value).hide();
+    }
+    mostrar(value){
+        $(value).show();
+    }
+    cambiarFondo(value,c){
+        $(value).css({left:0,backgroundColor:c});
+    }
+    cambiarColorDeLetra(value,c){
+        $(value).css({left:0,color:c});
+    }
+    añadirAcontinuacion(value){
+        $(value).append("Este es en la misma linea");
+    }
+    añadirLineaAntes(value){
+        $(value).prepend("SE AÑADIO ANTES");
+    }
+    añadirAntesH3(value){
+        $(value).before("<h3>Aquí tienes el nuevo texto before</h3>");
+    }
+    añadirDespuesH3(value){
+        $(value).after("<h3>Aquí tienes el nuevo texto after</h3>");
+    }
+    añadirFila(value){
+        $(value).append("<tr><td>111</td><td>112</td><td>113</td><td>114</td></tr>")
+    }
+    eliminar(value){
+        $(value).remove();
+    }
+    añadirElemento(value){
+        $(value).after("<li>Esta serie tuvo mucho éxito</li>");
+    }
+    añadirElementoHijo(value){
+        $(value).after("<ul><li>Esta serie tuvo mucho éxito</li></ul>");
+    }
+    ver(){
+        $("*", document.body).each(function() {
+            var etiquetaPadre = $(this).parent().get(0).tagName;
+            $(this).prepend(document.createTextNode( "Etiqueta padre : <"  + etiquetaPadre + "> elemento : <" + $(this).get(0).tagName +"> valor: "));
+        });
+    }
+    sumar(value){
+        var suma = 0;
+        $(value + ' td').each(function () {
+            suma += parseInt($(this).html());
+        });
+        alert("Resultado: " + suma);             
+    }
+}
+var computacion = new Computacion();
