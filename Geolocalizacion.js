f7ebecaa4c343b08d06fee5d490ec9b24373d7ca
uo282834 <113586636+uo282834@users.noreply.github.com>
2022-12-06 22:26:14 +0100

    Add files via upload

--- Geolocalizacion.js
+++ Geolocalizacion.js
@@ -1,7 +1,41 @@
-var mapaDinamicoGoogle = new Object();
-function initMap(){
-    var elentrego = {lat: 43.286806, lng: -5.637305};
-    var mapaElEntrego = new google.maps.Map(document.getElementsByTagName('main')[0],{zoom: 8,center:elentrego});
-    var marcador = new google.maps.Marker({position:elentrego,map:mapaElEntrego});
-}
-mapaDinamicoGoogle.initMap = initMap;
+var miMapa = new Object();
+
+function initMap(){  
+    var centro = {lat: 43.3672702, lng: -5.8502461};
+    var mapaGeoposicionado = new google.maps.Map(document.getElementsByTagName('main')[0],{
+        zoom: 8,
+        center:centro,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    
+    infoWindow = new google.maps.InfoWindow;
+    if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Localización encontrada');
+            infoWindow.open(mapaGeoposicionado);
+            mapaGeoposicionado.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, mapaGeoposicionado.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, mapaGeoposicionado.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: Ha fallado la geolocalización' :
+                              'Error: Su navegador no soporta geolocalización');
+        infoWindow.open(mapaGeoposicionado);
+      }
+
+miMapa.initMap = initMap;
+
