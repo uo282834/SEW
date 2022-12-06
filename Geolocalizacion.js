257bc6b4888bf207118848958fb1a0fde6992619
uo282834 <113586636+uo282834@users.noreply.github.com>
2022-12-06 21:53:17 +0100

    Add files via upload

--- Geolocalizacion.js
+++ Geolocalizacion.js
@@ -1,13 +1,10 @@
 "use strict";
-class Geolocalizacion {
+class Geolocalización {
     constructor (){
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this));
-        } else {
-            x.innerHTML = "Geolocation is not supported by this browser.";
-        }
+        navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this), this.verErrores.bind(this));
     }
     getPosicion(posicion){
+        this.mensaje = "Se ha realizado correctamente la petición de geolocalización";
         this.longitud         = posicion.coords.longitude; 
         this.latitud          = posicion.coords.latitude;  
         this.precision        = posicion.coords.accuracy;
@@ -15,6 +12,22 @@ class Geolocalizacion {
         this.precisionAltitud = posicion.coords.altitudeAccuracy;
         this.rumbo            = posicion.coords.heading;
         this.velocidad        = posicion.coords.speed;       
+    }
+    verErrores(error){
+        switch(error.code) {
+        case error.PERMISSION_DENIED:
+            this.mensaje = "El usuario no permite la petición de geolocalización"
+            break;
+        case error.POSITION_UNAVAILABLE:
+            this.mensaje = "Información de geolocalización no disponible"
+            break;
+        case error.TIMEOUT:
+            this.mensaje = "La petición de geolocalización ha caducado"
+            break;
+        case error.UNKNOWN_ERROR:
+            this.mensaje = "Se ha producido un error desconocido"
+            break;
+        }
     }
     getLongitud(){
         return this.longitud;
@@ -27,16 +40,16 @@ class Geolocalizacion {
     }
     verTodo(dondeVerlo){
         var ubicacion=document.getElementsByTagName(dondeVerlo)[0];
-        var datos='';
-        datos+='<h2>Ubicación</h2>';  
+        var datos='<p>'+ this.mensaje + '</p>'; 
+           datos+='<h2>Ubicación: </h2>'; 
         datos+='<p>Longitud: '+this.longitud +' grados</p>'; 
         datos+='<p>Latitud: '+this.latitud +' grados</p>';
-        datos+='<p>Precisión de la latitud y longitud: '+ this.precision +' metros</p>';
-         datos+='<p>Altitud: '+ this.altitude +' metros</p>';
+        datos+='<p>Precisión de la longitud y latitud: '+ this.precision +' metros</p>';
+        datos+='<p>Altitud: '+ this.altitude +' metros</p>';
         datos+='<p>Precisión de la altitud: '+ this.precisionAltitud +' metros</p>'; 
         datos+='<p>Rumbo: '+ this.rumbo +' grados</p>'; 
-        datos+='<p>Velocidad: '+ this.velocidad +' metros/segundo</p>'; 
+        datos+='<p>Velocidad: '+ this.velocidad +' metros/segundo</p>';
         ubicacion.innerHTML = datos;
     }
 }
-var miPosicion = new Geolocalizacion();
+var miPosicion = new Geolocalización();
