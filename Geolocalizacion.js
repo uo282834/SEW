70de78a35958c7f071156dca0cafcfd7f9435d8a
uo282834 <113586636+uo282834@users.noreply.github.com>
2022-12-06 22:34:36 +0100

    Add files via upload

--- Geolocalizacion.js
+++ Geolocalizacion.js
@@ -1,6 +1,6 @@
 var miMapa = new Object();
 
-function initMap(){  
+function initMapTrafico(){  
     var centro = {lat: 43.3672702, lng: -5.8502461};
     var mapaGeoposicionado = new google.maps.Map(document.getElementsByTagName('main')[0],{
         zoom: 8,
@@ -20,6 +20,7 @@ function initMap(){
             infoWindow.setContent('Localización encontrada');
             infoWindow.open(mapaGeoposicionado);
             mapaGeoposicionado.setCenter(pos);
+            mapaGeoposicionado.setZoom(15);
           }, function() {
             handleLocationError(true, infoWindow, mapaGeoposicionado.getCenter());
           });
@@ -27,7 +28,45 @@ function initMap(){
           // Browser doesn't support Geolocation
           handleLocationError(false, infoWindow, mapaGeoposicionado.getCenter());
         }
+
+        var trafficLayer = new google.maps.TrafficLayer();
+        trafficLayer.setMap(mapaGeoposicionado);
+
+
       }
+
+      function initMapTransporte(){  
+        var centro = {lat: 43.3672702, lng: -5.8502461};
+        var mapaGeoposicionado = new google.maps.Map(document.getElementsByTagName('main')[0],{
+            zoom: 8,
+            center:centro,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+        
+        infoWindow = new google.maps.InfoWindow;
+        if (navigator.geolocation) {
+              navigator.geolocation.getCurrentPosition(function(position) {
+                var pos = {
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude
+                };
+    
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('Localización encontrada');
+                infoWindow.open(mapaGeoposicionado);
+                mapaGeoposicionado.setCenter(pos);
+                mapaGeoposicionado.setZoom(15);
+              }, function() {
+                handleLocationError(true, infoWindow, mapaGeoposicionado.getCenter());
+              });
+            } else {
+              // Browser doesn't support Geolocation
+              handleLocationError(false, infoWindow, mapaGeoposicionado.getCenter());
+            }
+
+            var transitLayer = new google.maps.TransitLayer();
+            transitLayer.setMap(mapaGeoposicionado);
+        }    
 
       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
@@ -37,5 +76,6 @@ function initMap(){
         infoWindow.open(mapaGeoposicionado);
       }
 
-miMapa.initMap = initMap;
+miMapa.initMapTrafico = initMapTrafico;
+miMapa.initMapTransporte = initMapTransporte;
 
